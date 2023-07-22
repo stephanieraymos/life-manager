6d0b3b289bb2994f2291ce4b27e11cb443f27dc4
stephanieraymos <stephanieraymos@gmail.com>
2023-07-22 00:25:07 -0700

    project page styles

--- src/components/MyProjects.vue
+++ src/components/MyProjects.vue
@@ -1,15 +1,19 @@
 <template>
     <div class="container">
-        My Projects
-        <div v-if="loading">Loading...</div>
+        <h2>My Projects</h2>
+        <div v-if="loading" class="loading">Loading...</div>
         <div v-else>
-            <div
-                v-for="repo in appStore.githubRepos"
-                :key="repo.id"
-                @click="viewRepoDetails(repo)"
-            >
-                {{ repo.name }}
+            <div class="repository-list">
+                <div
+                    v-for="repo in appStore.githubRepos"
+                    :key="repo.id"
+                    class="repository-list-item"
+                    @click="viewRepoDetails(repo)"
+                >
+                    {{ repo.name }}
+                </div>
             </div>
+            <router-view> </router-view>
         </div>
         <!-- <router-link to="/details">Go to det</router-link>; -->
         <router-view></router-view>
@@ -39,10 +43,31 @@ const viewRepoDetails = repo => {
 
 <style scoped>
 .container {
+    background: var(--dark-color-2);
+
+    margin: 10px;
+    border-radius: 10px;
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    margin-top: 60px;
+    margin-top: 20px;
+}
+.loading {
+    margin-top: 20px;
+}
+.repository-list {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    margin: 20px;
+}
+.repository-list-item {
+    background: var(--color-blue);
+
+    cursor: pointer;
+    border-radius: 10px;
+    width: max-content;
+    padding: 10px;
+    margin: 10px;
 }
 </style>
--- src/components/RepositoryDetails.vue
+++ src/components/RepositoryDetails.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="container">
+        <router-link class="link" to="/projects"
+            >&lt; Back to my projects</router-link
+        >
         <h2>Repo Details</h2>
         <div><strong>Repository Name:</strong> {{ $route.params.slug }}</div>
     </div>
@@ -9,10 +12,19 @@
 
 <style scoped>
 .container {
+    position: relative;
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     margin-top: 60px;
+}
+.link {
+    position: absolute;
+    top: 0;
+    left: 0;
+    margin: 0 10px;
+    text-decoration: none;
+    color: #fff;
 }
 </style>
